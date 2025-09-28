7fc314cef92ff206bcb7b7fcb9cb7895a0d9927c
Yuuki Shimizu <yuuki@maxio.jp>
2025-09-28 14:19:05 +0900

    add date parameters for search requests

--- app.rb
+++ app.rb
@@ -23,9 +23,23 @@ get '/:service' do
 
   halt 500, "No search words." if words.empty?
 
+  date_query_parts = []
+  
+  if params[:since]
+    date_query_parts << "since:#{params[:since]}_JST"
+  end
+  
+  if params[:until]
+    date_query_parts << "until:#{params[:until]}_JST"
+  end
+
   case service
   when 'x'
-    query = words.join(" OR ")
+    query_parts = []
+    query_parts.concat(date_query_parts) unless date_query_parts.empty?
+    query_parts << words.join(" OR ")
+    
+    query = query_parts.join(" ")
     q = CGI.escape(query)
     redirect "#{base_url}#{q}", 302
   end
